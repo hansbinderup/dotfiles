628ef15367c46816e2f36ff321045343e1b87a26
Hans Binderup <habi@bang-olufsen.dk>
2024-07-07 15:26:32 +0200

    nvim: minor updates
    Signed-off-by: Hans Binderup <habi@bang-olufsen.dk>

--- nvim/plugin/telescope.lua
+++ nvim/plugin/telescope.lua
@@ -19,7 +19,7 @@ local ignore_big_files_maker = function(filepath, bufnr, opts)
   filepath = vim.fn.expand(filepath)
   vim.loop.fs_stat(filepath, function(_, stat)
     if not stat then return end
-    if stat.size > 100000 then
+    if stat.size > 500000 then
       return
     else
       previewers.buffer_previewer_maker(filepath, bufnr, opts)
--- nvim/plugin/treesitter.lua
+++ nvim/plugin/treesitter.lua
@@ -1,39 +1,38 @@
-local status_ok, treesitter = pcall(require, "nvim-treesitter")
-if not status_ok then
-	return
-end
+ local status_ok, treesitter = pcall(require, "nvim-treesitter")
+ if not status_ok then
+ 	return
+ end
 
-local status_ok, configs = pcall(require, "nvim-treesitter.configs")
-if not status_ok then
-    vim.notify("Error loading nvim-treesitter.configs")
-    return
-end
+ local status_ok, configs = pcall(require, "nvim-treesitter.configs")
+ if not status_ok then
+     vim.notify("Error loading nvim-treesitter.configs")
+     return
+ end
 
-configs.setup {
-    ensure_installed = "all",
-    sync_install = false,
-    ignore_install = {}, -- List of parsers to ignore installing
-    highlight = {
-        enable = true,
-        disable = function(lang, bufnr) -- Disable in large C++ buffers
-            return lang == "cpp" and api.nvim_buf_line_count(bufnr) > 5000
-        end,
-        disable = { "" }, -- list of language that will be disabled
-        additional_vim_regex_highlighting = true
-    },
-    indent = {
-        enable = true,
-        disable = { "yaml" }
-    },
-    autopairs = {
-        enable = true
-    },
-    context_commentstring = {
-        enable = true,
-        enable_autocmd = false
-    },
-    playground = {
-        enable = true
-    },
-}
+ configs.setup {
+     ensure_installed = {"c", "cpp", "lua", "bash"},
+     sync_install = false,
+     ignore_install = {}, -- List of parsers to ignore installing
+     highlight = {
+         enable = true,
+         disable = function(lang, bufnr) -- Disable in large C++ buffers
+             return lang == "cpp" and api.nvim_buf_line_count(bufnr) > 5000
+         end,
+         disable = { "" }, -- list of language that will be disabled
+         additional_vim_regex_highlighting = true
+     },
+     indent = {
+         enable = true,
+     },
+     autopairs = {
+         enable = true
+     },
+     context_commentstring = {
+         enable = true,
+         enable_autocmd = false
+     },
+     playground = {
+         enable = true
+     },
+ }
 
