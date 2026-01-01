89d902d85ca8fad66c4b35be7918a15bf59a9898
Hans Binderup <habi@bang-olufsen.dk>
2026-01-01 16:22:47 +0100

    lazygit: add initial support
    Signed-off-by: Hans Binderup <habi@bang-olufsen.dk>

---
+++ nvim/lua/plugins/lazygit.lua
@@ -0,0 +1,18 @@
+return {
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    cmd = {
+        "LazyGit",
+        "LazyGitConfig",
+        "LazyGitCurrentFile",
+        "LazyGitFilter",
+        "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+    keys = {
+        { "<leader>git", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+    }
+}
