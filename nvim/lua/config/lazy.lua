052cbac61c5722be99202bfb1110f85dfcdb40c2
Hans Binderup <habi@bang-olufsen.dk>
2026-01-13 08:53:30 +0100

    nvim: lazy: don't notify on updates
    Rather annoying when you open a lot of nvim servers. I'd rather check
    myself..
    
    Signed-off-by: Hans Binderup <habi@bang-olufsen.dk>

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -31,5 +31,8 @@ require("lazy").setup({
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "gruvbox" } },
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  checker = {
+        enabled = true,
+        notify = false
+  },
 })
