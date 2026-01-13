6cddba4eb78fdd4bd2ba7d58e9361daa19c41208
Hans Binderup <habi@bang-olufsen.dk>
2026-01-13 08:55:10 +0100

    nvim: plugin: add comment plugin
    Was missing from the old config.
    
    Signed-off-by: Hans Binderup <habi@bang-olufsen.dk>

---
+++ nvim/lua/plugins/comment.lua
@@ -0,0 +1,13 @@
+return {
+    'numToStr/Comment.nvim',
+    opts = {
+        toggler = {
+            ---Line-comment toggle keymap
+            line = '<C-k>',
+        },
+        opleader = {
+            ---Line-comment keymap
+            line = '<C-k>',
+        },
+    },
+}
