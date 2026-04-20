f1d53e171a2aa1cb267d037b032075bb0be18403
Hans Binderup <habi@bang-olufsen.dk>
2026-04-20 19:03:27 +0200

    backup
    Signed-off-by: Hans Binderup <habi@bang-olufsen.dk>

--- nvim/lua/plugins/cscope.lua
+++ nvim/lua/plugins/cscope.lua
@@ -1,18 +1,19 @@
 return {
-    {
-        'dhananjaylatkar/cscope_maps.nvim',
-        dependencies = {
-            'nvim-telescope/telescope.nvim',
-        },
-        lazy = false,
-        config = function()
-            require("cscope_maps").setup {
-                skip_input_prompt = true,
-                cscope = {
-                    picker = "telescope",
-                    skip_picker_for_single_result = true,
-                }
-            }
-        end
-    }
+    -- {
+    --     'dhananjaylatkar/cscope_maps.nvim',
+    --     ft = { "c" },  -- Only load for C files
+    --     dependencies = {
+    --         'nvim-telescope/telescope.nvim',
+    --     },
+    --     lazy = false,
+    --     config = function()
+    --         require("cscope_maps").setup {
+    --             skip_input_prompt = true,
+    --             cscope = {
+    --                 picker = "telescope",
+    --                 skip_picker_for_single_result = true,
+    --             }
+    --         }
+    --     end
+    -- }
 }
