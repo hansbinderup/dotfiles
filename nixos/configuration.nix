87ba2933b1a401b5cd4e0e6fb95ec58d16e22336
Hans Binderup <habi@bang-olufsen.dk>
2026-04-22 15:59:57 +0200

    nixos: isolate audio
    Move all audio related configs to its own nix config.
    
    Signed-off-by: Hans Binderup <habi@bang-olufsen.dk>

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -13,6 +13,7 @@
       ./services/tmux.nix
       ./services/github-copilot.nix
       ./services/openconnect.nix
+      ./services/audio.nix
     ];
 
   nixpkgs.config.allowUnfree = true;
@@ -34,15 +35,6 @@
   hardware.enableRedistributableFirmware = true;
   hardware.bluetooth.enable = true;
   hardware.bluetooth.powerOnBoot = true;
-
-  # rtkit (optional, recommended) allows Pipewire to use the realtime scheduler for increased performance.
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    pulse.enable = true;
-    alsa.enable = true;
-    wireplumber.enable = true;
-  };
 
   # increase ulimit
   security.pam.loginLimits = [{
@@ -83,9 +75,6 @@
     libgcc
     gcc-arm-embedded
     fastfetch
-    pavucontrol
-    playerctl
-    pamixer
     wl-clipboard
     grim
     slurp
